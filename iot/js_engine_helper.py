ce5341c4df8a4ab95e2c98ee3d5a5cd3466e59b1
Rodrigo <rodrigomorales2@acm.org>
2018-09-01 16:01:44 -0400

    adding hiv computation to nsgaii_full_settings and adding a dictionary to avoid reevaluating solutions

--- iot/js_engine_helper.py
+++ iot/js_engine_helper.py
@@ -1,6 +1,7 @@
 import configparser
 import sys,  os, subprocess, csv
 from collections import deque
+from random import random
 
 """
 .. module:: miniaturization
@@ -11,12 +12,10 @@ from collections import deque
 .. moduleauthor:: Rodrigo Morales <rodrigomorales2@acm.org>
 """
 
-class ProgramPerformanceMetrics ():
+class ProgramPerformanceMetrics:
     ''' In this class we define the properties of the program to optimize'''
-
     def __init__(self) -> None:
-         super().__init__()
-    code_size, memory_us, execution_time = [],[],[]
+        self.code_size, self.memory_us, self.execution_time = [],[],[]
     # @property
     # def code_size_delta(self,code_size_org) -> float:
     #     delta()
@@ -112,6 +111,7 @@ class JSEngineHelper ():
         self.plog = logError
         self.cwd = cwd
         self.bc = bc
+        self.tested_solutions = {}
 
 
     defaultSolution = []
@@ -122,11 +122,17 @@ class JSEngineHelper ():
 
         # f7-f10 requires to be disable together (one-index-based)
         if asolution[6]==True or asolution[7]==True or asolution[8]==True:
-            asolution = self.defaultSolution.copy()
-            asolution[6] = True
-            asolution[7] = True
-            asolution[8] = True
-            asolution[9] = False
+            if random()<0.2:
+                asolution = self.defaultSolution.copy()
+                asolution[6] = True
+                asolution[7] = True
+                asolution[8] = True
+                asolution[9] = False
+            else:
+                asolution[6] = False
+                asolution[7] = False
+                asolution[8] = False
+                #asolution[9] = True
 
         # f14 requires f11 to be disable too (one-index-based)
         if asolution[13]==False:
@@ -159,78 +165,86 @@ class JSEngineHelper ():
 
 
     def evaluate_solution_performance_(self, asolution):
-        '''Find the positions in which asolution differs from default solution'''
-        differences = [
-            (inner_idx)
-            for inner_idx, (a_element, b_element) in enumerate(zip(asolution, self.defaultSolution))
-            if a_element != b_element
-        ]
-
-        '''Now let's iterate over the differences to generate a new config.yaml file'''
-        os.system('mkdir -p ' + self.cwd + '/configFiles/' + self.bc.idf)
-        fileoutname =  self.cwd +"/configFiles/" + self.bc.idf + "/" + self.bc.idf + ".yaml"
-        fout = open(fileoutname, "w")
-        for feature_idx in differences:
-            features = list(self.feature_list.items())
-            feature = features[feature_idx][1][0] + ': ' + features[feature_idx][1][2]
-            fout.write(feature + "\n")
-        fout.close()
-        '''Now we need to compile the new config file'''
-        os.system('mkdir -p ' + self.cwd + '/duktape-src/' + self.bc.idf)
-        os.system('rm -f ' + self.cwd + '/duktape-src/' + self.bc.idf + '/*')
-        '''Because using ROM requires special parameter for the config script
-            we detect when this happens'''
-        if asolution[6]==True:
-            os.system('python '+ self.bc.duk_path+ '/tools/configure.py --output-directory ' + \
-                      self.cwd + '/duktape-src/' + self.bc.idf + ' --rom-support --option-file ' +  fileoutname)
-        else:
-            os.system('python '+ self.bc.duk_path+ '/tools/configure.py --output-directory ' + \
-                       self.cwd + '/duktape-src/' + self.bc.idf + ' --option-file '  + fileoutname)
-        ###Copy the source code, since I have problems when compiling when the headers are in a different dir
-        ###Now the next step is to compile the code
-        os.system('cp -f ' + self.cwd + '/{' + self.bc.program + '.c,' + self.bc.script + '} ' + self.cwd + '/duktape-src/' + self.bc.idf + '/')
-        os.chdir(self.cwd + '/duktape-src/' + self.bc.idf + '/')
-        compileSucc = os.system('gcc -std=c99 -o ' + self.bc.program + ' -Iduktape-src duktape.c ' + self.bc.program + '.c -lm')
-        if compileSucc != 0:
-            self.plog.logError("When compiling program: " + self.bc.program + ". configuration file: " + fileoutname + "\n")
-            sys.exit()
-        ####Now let's measure the size of the file
-        returned_out = subprocess.check_output(["stat", "-c", "\"%s\"", self.bc.program])
-        parsed = False
-        try:
-            feature_size = float(returned_out.decode("utf-8").replace("\"", "").strip())
-            parsed = True
-        except ValueError:
-            # here we should log an error with the corresponding feature
-            self.plog.logError(
-                "When measuring file size: " + self.bc.program + ". configuration file: " + fileoutname + ". errorMsg: " + returned_out.decode(
-                    "utf-8").replace("\"", "").strip() + "\n")
-            sys.exit()
         ''' Create a new object to record the measurements'''
-        ppm = ProgramPerformanceMetrics ()
-        ppm.code_size.append(feature_size)
-        """ Now it is time to execute the script to the harness and collect memory usage and execution time """
-        time_lines_count = 1  # how many lines /usr/bin/time produces
-        for count in range(0, int(self.bc.runs)+1):
-            p = subprocess.Popen(['/usr/bin/time', '-f \" %U , %S , %P\"', \
-                                  './' + self.bc.program, self.bc.script, self.bc.jsfunction], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            with p.stdout:
-                r = deque(iter(p.stdout.readline, b''), maxlen=time_lines_count)
-            with p.stderr:
-                q = deque(iter(p.stderr.readline, b''), maxlen=time_lines_count)
-            rc = p.wait()
-            # print(b''.join(q).decode().strip())
-            coltemp = str(b''.join(r).decode().strip()).split(',') + str(b''.join(q).decode().strip()).split(',')
-            print  (coltemp)
+        ppm = ProgramPerformanceMetrics()
+        ''' validate if the solution was not already evaluated to avoid reevaluting solutions'''
+        string_sol = ''.join(str(int(e)) for e in asolution)
+        decimal_rep_asolution = int(string_sol,2)
+        if decimal_rep_asolution in self.tested_solutions:
+            ppm.code_size =  self.tested_solutions.get(decimal_rep_asolution)[0]
+            ppm.memory_us = self.tested_solutions.get(decimal_rep_asolution)[1]
+            ppm.execution_time = self.tested_solutions.get(decimal_rep_asolution)[2]
+        else:
+            '''Find the positions in which asolution differs from default solution'''
+            differences = [
+                (inner_idx)
+                for inner_idx, (a_element, b_element) in enumerate(zip(asolution, self.defaultSolution))
+                if a_element != b_element
+            ]
+
+            '''Now let's iterate over the differences to generate a new config.yaml file'''
+            os.system('mkdir -p ' + self.cwd + '/configFiles/' + self.bc.idf)
+            fileoutname =  self.cwd +"/configFiles/" + self.bc.idf + "/" + self.bc.idf + ".yaml"
+            fout = open(fileoutname, "w")
+            for feature_idx in differences:
+                features = list(self.feature_list.items())
+                feature = features[feature_idx][1][0] + ': ' + features[feature_idx][1][2]
+                fout.write(feature + "\n")
+            fout.close()
+            '''Now we need to compile the new config file'''
+            os.system('mkdir -p ' + self.cwd + '/duktape-src/' + self.bc.idf)
+            os.system('rm -f ' + self.cwd + '/duktape-src/' + self.bc.idf + '/*')
+            '''Because using ROM requires special parameter for the config script
+                we detect when this happens'''
+            if asolution[6]==True:
+                os.system('python '+ self.bc.duk_path+ '/tools/configure.py --output-directory ' + \
+                          self.cwd + '/duktape-src/' + self.bc.idf + ' --rom-support --option-file ' +  fileoutname)
+            else:
+                os.system('python '+ self.bc.duk_path+ '/tools/configure.py --output-directory ' + \
+                           self.cwd + '/duktape-src/' + self.bc.idf + ' --option-file '  + fileoutname)
+            ###Copy the source code, since I have problems when compiling when the headers are in a different dir
+            ###Now the next step is to compile the code
+            os.system('cp -f ' + self.cwd + '/{' + self.bc.program + '.c,' + self.bc.script + '} ' + self.cwd + '/duktape-src/' + self.bc.idf + '/')
+            os.chdir(self.cwd + '/duktape-src/' + self.bc.idf + '/')
+            compileSucc = os.system('gcc -std=c99 -o ' + self.bc.program + ' -Iduktape-src duktape.c ' + self.bc.program + '.c -lm')
+            if compileSucc != 0:
+                self.plog.logError("When compiling program: " + self.bc.program + ". configuration file: " + fileoutname + "\n")
+                sys.exit()
+            ####Now let's measure the size of the file
+            returned_out = subprocess.check_output(["stat", "-c", "\"%s\"", self.bc.program])
             try:
-                ppm.memory_us.append(float(coltemp[0].replace("\"", "").strip()))
-            except:
-                self.plog.logError("When executing program: " + self.bc.program + ". configuration file: " + fileoutname + ". errorMsg: " +
-                                   coltemp[0].replace("\"", "").strip() + "\n")
-                ppm.memory_us.append  (float('inf'))
-                break
-            ppm.execution_time.append(float(coltemp[1].replace("\"","").strip()))
-        os.chdir(self.cwd) #always return to the original path
+                feature_size = float(returned_out.decode("utf-8").replace("\"", "").strip())
+            except ValueError:
+                # here we should log an error with the corresponding feature
+                self.plog.logError(
+                    "When measuring file size: " + self.bc.program + ". configuration file: " + fileoutname + ". errorMsg: " + returned_out.decode(
+                        "utf-8").replace("\"", "").strip() + "\n")
+                sys.exit()
+
+            ppm.code_size.append(feature_size)
+            """ Now it is time to execute the script to the harness and collect memory usage and execution time """
+            time_lines_count = 1  # how many lines /usr/bin/time produces
+            for count in range(0, int(self.bc.runs)):
+                p = subprocess.Popen(['/usr/bin/time', '-f \" %U , %S , %P\"', \
+                                      './' + self.bc.program, self.bc.script, self.bc.jsfunction], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                with p.stdout:
+                    r = deque(iter(p.stdout.readline, b''), maxlen=time_lines_count)
+                with p.stderr:
+                    q = deque(iter(p.stderr.readline, b''), maxlen=time_lines_count)
+                rc = p.wait()
+                # print(b''.join(q).decode().strip())
+                coltemp = str(b''.join(r).decode().strip()).split(',') + str(b''.join(q).decode().strip()).split(',')
+                print  (coltemp)
+                try:
+                    ppm.memory_us.append(float(coltemp[0].replace("\"", "").strip()))
+                except:
+                    self.plog.logError("When executing program: " + self.bc.program + ". configuration file: " + fileoutname + ". errorMsg: " +
+                                       coltemp[0].replace("\"", "").strip() + "\n")
+                    ppm.memory_us.append  (float('inf'))
+                    break
+                ppm.execution_time.append(float(coltemp[1].replace("\"","").strip()))
+            os.chdir(self.cwd) #always return to the original path
+            self.tested_solutions[decimal_rep_asolution]= [ppm.code_size,ppm.memory_us,ppm.execution_time]
         return ppm
 
 
--- iot/nsgaii_full_settings.py
+++ iot/nsgaii_full_settings.py
@@ -2,7 +2,8 @@ from jmetal.algorithm import NSGAII
 from jmetal.component import VisualizerObserver, ProgressBarObserver, RankingAndCrowdingDistanceComparator
 from miniaturization import Miniaturization
 from jmetal.operator import SP, BitFlip, BinaryTournamentSelection
-from jmetal.util import ScatterMatplotlib, SolutionList
+from jmetal.util import  SolutionList
+from jmetal.component.quality_indicator import HyperVolume
 
 
 if __name__ == '__main__':
@@ -17,22 +18,27 @@ if __name__ == '__main__':
         selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator())
     )
 
-    observer = VisualizerObserver(problem)
-    progress_bar = ProgressBarObserver(step=100, maximum=25000)
-    algorithm.observable.register(observer=observer)
+    #observer = VisualizerObserver(problem)
+    progress_bar = ProgressBarObserver(step=10, maximum=250)
+    #algorithm.observable.register(observer=observer)
     algorithm.observable.register(observer=progress_bar)
 
     algorithm.run()
     front = algorithm.get_result()
 
     # Plot frontier to file
-    pareto_front = ScatterMatplotlib(plot_title='NSGAII for IoT-Min', number_of_objectives=problem.number_of_objectives)
-    pareto_front.plot(front, reference=problem.get_reference_front(), output='NSGAII-IoT-Min', show=False)
+    #pareto_front = ScatterMatplotlib(plot_title='NSGAII for IoT-Min', number_of_objectives=problem.number_of_objectives)
+    #pareto_front.plot(front, reference=problem.get_reference_front(), output='NSGAII-IoT-Min', show=False)
 
     # Save variables to file
     SolutionList.print_function_values_to_file(front, 'FUN.NSGAII.' + problem.get_name())
     SolutionList.print_variables_to_file(front, 'VAR.NSGAII.' + problem.get_name())
 
+    reference_point = [1, 1, 1 ,1]
+    hv = HyperVolume(reference_point)
+    value = hv.compute(problem.get_reference_front())
     print('Algorithm (binary problem): ' + algorithm.get_name())
     print('Problem: ' + problem.get_name())
+    print ('HyperVolume: %f' % value)
     print('Computing time: ' + str(algorithm.total_computing_time))
+    problem.sf.plog.logError('Repeated solutions:'+str(len(problem.sf.js_engine_helper.tested_solutions))+'\n')
